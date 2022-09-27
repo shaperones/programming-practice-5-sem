6587779ef8634a442a51b5eb5b81b0879464a427
anastasiia latypova <70793408+shaperones@users.noreply.github.com>
2022-09-28 01:01:17 +0300

    HW4
    hw for lesson 4 added

---
+++ HW4/complex_numbers_2.py
@@ -0,0 +1,219 @@
+''' Доработайте класс комплексного числа из прошлого занятия: переопределите математические
+операторы (+, -, /, *, ==), так, чтобы она работали с другими комплексными и со стандартными
+числами. Добейтесь правилньой работы с комплексным числом функций print(), abs(), а также
+реализуйте обращение к действительной и мнимой части комплексного числа через x[0] и x[1]
+соответственно (как для получения так и для присваивания).'''
+
+import numbers
+import numpy as np
+
+
+class Complex:  # стандартная форма: a + bi 'standard', экспоненциальная: r*exp(fi) 'exponential'
+    def __init__(self, a=0.0, b=0.0, r=0.0, f=0.0, form='standard'):
+        self.a = a
+        self.b = b
+        self.r = r
+        self.f = f
+        self.form = form
+
+    def get_form(self):
+        return self.form
+
+    def get_ab(self):  # геттер для стандартной формы
+        if self.form == 'standard':
+            return self.a, self.b
+        else:
+            return 'wrong form'
+
+    def get_rf(self):  # геттер для экспоненциальной формы
+        if self.form == 'exponential':
+            return self.r, self.f
+        else:
+            return 'wrong form'
+
+    def set_form(self, form):  # разрешаю использовать, чтобы задать форму при вводе числа, а не менять ее
+        if (self.form == 'standard' and self.a == 0 and self.b == 0) or (
+                self.form == 'exponential' and self.r == 0 and self.f == 0):
+            self.form = form
+        else:
+            print('ur number is already in one of the forms, use ~to_form~')
+
+    def set_ab(self, a, b):  # сеттеры для стандартной формы
+        if self.form == 'standard':
+            self.a = a
+            self.b = b
+        else:
+            print('wrong form')
+
+    def set_rf(self, r, f):  # сеттеры для экспоненциальной формы
+        if self.form == 'exponential':
+            self.r = r
+            self.f = f
+        else:
+            print('lol wrong form')
+
+    def to_expon_form(self):  # в экспоненциальную форму
+        if self.form == 'standard':
+            self.r = (self.a ** 2 + self.b ** 2) ** 0.5
+            self.f = np.arctan(self.b / self.a)
+            self.a = 0
+            self.b = 0
+            self.form = 'exponential'
+
+    def to_standard_form(self):  # из экспоненциальной в стандартную
+        if self.form == 'exponential':
+            self.a = self.r * np.cos(self.f)
+            self.b = self.r * np.sin(self.f)
+            self.r = 0
+            self.f = 0
+            self.form = 'standard'
+
+    def __add__(self, other):  # сложение
+        if isinstance(other, Complex):
+            if self.form == 'standard' and other.form == 'standard':
+                return Complex(a=(self.a + other.a), b=(self.b + other.b))
+            else:
+                print('complex numbers should be in the standard form')
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                return Complex(a=(self.a + other), b=self.b)
+            else:
+                print('complex numbers should be in the standard form')
+
+    def __radd__(self, other):  # сложение (не добавляю вариант с 2 комплексными, тк он покрывается __add__)
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                return Complex(a=(self.a + other), b=self.b)
+            else:
+                print('complex numbers should be in the standard form')
+
+    def __sub__(self, other):  # вычитание
+        if isinstance(other, Complex):
+            if self.form == 'standard' and other.form == 'standard':
+                return Complex(a=(self.a - other.a), b=(self.b - other.b))
+            else:
+                print('complex numbers should be in the standard form')
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                return Complex(a=(self.a - other), b=self.b)
+            else:
+                print('complex numbers should be in the standard form')
+
+    def __rsub__(self, other):  # вычитание number - complex
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                return Complex(a=(other - self.a), b=(-self.b))
+            else:
+                print('complex numbers should be in the standard form')
+
+    def __mul__(self, other):  # умножение
+        if isinstance(other, Complex):
+            if self.form == 'standard' and other.form == 'standard':
+                return Complex(a=(self.a * other.a - self.b * other.b), b=(self.a * other.b + self.b * other.a))
+            else:
+                print('complex numbers should be in the standard form')
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                return Complex(a=(self.a * other), b=(self.b * other))
+            else:
+                print('complex numbers should be in the standard form')
+
+    def __rmul__(self, other):  # умножение number * complex
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                return Complex(a=(self.a * other), b=(self.b * other))
+            else:
+                print('complex numbers should be in the standard form')
+
+    def __truediv__(self, other):  # деление
+        if isinstance(other, Complex):
+            if self.form == 'standard' and other.form == 'standard':
+                return Complex(a=((self.a * other.a + self.b * other.b) / (other.a ** 2 + other.b ** 2)),
+                               b=((self.b * other.a - self.a * other.b) / (other.a ** 2 + other.b ** 2)))
+            else:
+                print('numbers should be in the standard form')
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                return Complex(a=(self.a / other), b=(self.b / other))
+            else:
+                print('complex numbers should be in the standard form')
+
+    def __rtruediv__(self, other):  # деление number / complex
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                return Complex(a=(self.a * other / (self.a ** 2 + self.b ** 2)),
+                               b=(- self.b * other / (self.a ** 2 + self.b ** 2)))
+            else:
+                print('complex numbers should be in the standard form')
+
+    def __eq__(self, other):  # сравнение
+        if isinstance(other, Complex):
+            if self.form == 'standard' and other.form == 'standard':
+                if self.a == other.a and self.b == other.b:
+                    return True
+                else:
+                    return False
+            else:
+                print('complex numbers should be in the standard form')
+                return False
+        if isinstance(other, numbers.Number):
+            if self.form == 'standard':
+                if self.b == 0 and self.a == other:
+                    return True
+                else:
+                    return False
+            else:
+                print('complex numbers should be in the standard form')
+                return False
+
+    def __abs__(self):
+        if self.form == 'standard':
+            return (self.a ** 2 + self.b ** 2) ** 0.5
+        else:
+            print('complex numbers should be in the standard form')
+
+    def __str__(self):
+        if self.form == 'standard':
+            return str(self.a) + " " + str(self.b)
+        if self.form == 'exponential':
+            return str(self.r) + " " + str(self.f)
+
+    def __getitem__(self, item):
+        if self.form == 'exponential':
+            print('complex numbers should be in the standard form')
+        else:
+            if item == 0:
+                return self.a
+            if item == 1:
+                return self.b
+            else:
+                return False
+
+    def __setitem__(self, key, value):
+        if self.form == 'exponential':
+            print('complex numbers should be in the standard form')
+        else:
+            if key == 0:
+                self.a = value
+            if key == 1:
+                self.b = value
+            else:
+                return False
+
+
+# тесты
+
+n = Complex(a=1, b=1)
+n1 = Complex(a=2, b=1.5)
+n2 = Complex(a=3, b=0)
+
+print(n + n1, n1 + n, n + 2, 2 + n, n - n1, n1 - n, n - 2, 2 - n)
+print(n * n1, n1 * n, n * 2, 2 * n, n / n1, n1 / n, n / 2, 2 / n)
+print(n == n1, n == 3, n2 == 3)
+print(n[0], n[1], n[3])
+n[0] = 6
+n[1] = 0
+n[2] = 8
+print(n[0], n[1], abs(n2))
+
+
---
+++ HW4/list.py
@@ -0,0 +1,142 @@
+'''Доработайте класс двусвязного списка из классного примера: обращение к элементу
+по индексу должно занимать не более чем O(N/2) операций, создайте возможность
+инициализировать ваш лист заданным набором чисел, добавьте операцию выкидывания
+по индексу через метод pop() и переопределите оператор сложения двух листов как
+их конкатенацию.'''
+
+
+class Node:
+    def __init__(self, value,
+                 prev_pointer=None, next_pointer=None):
+        self.set_value(value)
+        self.set_prev(prev_pointer)
+        self.set_next(next_pointer)
+
+    def get_value(self):
+        return self._value
+
+    def get_next(self):
+        return self._next_pointer
+
+    def get_prev(self):
+        return self._prev_pointer
+
+    def set_value(self, value):
+        self._value = value
+
+    def set_prev(self, prev_pointer):
+        self._prev_pointer = prev_pointer
+
+    def set_next(self, next_pointer):
+        self._next_pointer = next_pointer
+
+    def __str__(self):
+        return str(self.get_value())
+
+
+class List:
+    def __init__(self, start=None, finish=None, length=0, values=[]):  # values: list
+        self._start_pointer = start
+        self._finish_pointer = finish
+        self._length = length
+        if len(values) != 0:
+            for value in values:
+                self.append(value)
+
+    def __len__(self):
+        return self._length
+
+    def append(self, value):
+        if self._length == 0:
+            self._start_pointer = Node(value)
+            self._finish_pointer = self._start_pointer
+            self._length = 1
+        else:
+            self._finish_pointer.set_next(Node(value,
+                                               self._finish_pointer))
+            self._finish_pointer = self._finish_pointer.get_next()
+            self._length += 1
+
+    def __getitem__(self, i):
+        if i < 0 or i >= self._length:
+            return False
+
+        if i <= len(self) / 2:
+            curr_pointer = self._start_pointer
+            for j in range(i):
+                curr_pointer = curr_pointer.get_next()
+            return curr_pointer.get_value()
+
+        if i > len(self) / 2:
+            curr_pointer = self._finish_pointer
+            for j in range(len(self)-1, i, -1):
+                curr_pointer = curr_pointer.get_prev()
+            return curr_pointer.get_value()
+
+    def __str__(self):
+        arr = []
+        for i in range(self._length):
+            arr.append(str(self[i]))
+        return "[" + ", ".join(arr) + "]"
+
+    def __add__(self, other):
+        if isinstance(other, List):
+            if len(self) != 0:
+                if len(other) != 0:
+                    c = List(start=self._start_pointer, finish=self._finish_pointer)
+                    c._finish_pointer.set_next(other._start_pointer)
+                    c._finish_pointer = other._finish_pointer
+                    c._length = len(self) + len(other)
+                    return c
+                else:
+                    return self
+            else:
+                return other
+        else:
+            return False
+
+    def pop(self, i):
+        if i < 0 or i >= len(self):
+            return False
+        else:
+            b = self[i]
+            if i == 0:
+                self._start_pointer = self._start_pointer.get_next()
+                self._start_pointer.set_prev(None)
+                self._length -= 1
+                return b
+            if i == (len(self) - 1):
+                self._finish_pointer = self._finish_pointer.get_prev()
+                self._finish_pointer.set_next(None)
+                self._length -= 1
+                return b
+            else:
+                if i <= len(self) / 2:
+                    curr_pointer = self._start_pointer
+                    for j in range(i):
+                        curr_pointer = curr_pointer.get_next()
+                if i > len(self) / 2:
+                    curr_pointer = self._finish_pointer
+                    for j in range(len(self) - 1, i, -1):
+                        curr_pointer = curr_pointer.get_prev()
+                curr_pointer.get_prev().set_next(curr_pointer.get_next())
+                curr_pointer.get_next().set_prev(curr_pointer.get_prev())
+                self._length -= 1
+            return b
+
+
+A = List()
+for i in range(5):
+    A.append(i)
+print(A)
+C = List()
+B = List(values=[5, 6, 7, 9])
+print(B)
+print(A + B)
+print(A)
+print(B)
+print(A + C)
+print(C + A)
+print((A + B)[3])
+print(C)
+print(A.pop(0), A.pop(1), A.pop(2), A)
